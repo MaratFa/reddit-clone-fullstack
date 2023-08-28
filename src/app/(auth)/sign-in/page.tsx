a01b044573c4945888e9159ab6064eb0770cee32
Marat <evolution99@rambler.ru>
2023-08-28 23:49:05 +0300

    Edited page.tsx in sign-in folder

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -9,9 +9,13 @@ const page: FC = () => {
             <div className='h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20'>
                 <Link
                     href='/'
-                    className={cn(buttonVariants({ variant: 'ghost' }))}>
-                        Home
+                    className={cn(
+                        buttonVariants({ variant: 'ghost' }),
+                        'self-start -mt-20')}>
+                    Home
                 </Link>
+
+                <SignIn />
             </div>
         </div>
     )
